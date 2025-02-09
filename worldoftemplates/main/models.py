db6ee20e65d5ffbb10a30a4a69b01c16d37a06ea
Nodirbek Xakimov <xakimovn60@gmail.com>
2025-02-09 20:54:55 +0500

    Up to date

--- worldoftemplates/main/models.py
+++ worldoftemplates/main/models.py
@@ -9,7 +9,6 @@ class Customer(models.Model):
     last_name = models.CharField(max_length=200, null=True)
     email = models.EmailField(null=True, unique=True)
     password = models.CharField(max_length=100, null=True)
-    new_column = models.BigIntegerField(null=True, blank=True)
 
     def __str__(self):
         return self.username if self.username else "Unnamed Customer"
